be39bb8e6174067d2f582720c2a11b855d2f7f4f
农药君 <99505726+cnbilinyj@users.noreply.github.com>
2024-10-18 21:32:52 +0800

    udtms

--- WCSSMAMaS/tms-main-pigeonhole.js
+++ WCSSMAMaS/tms-main-pigeonhole.js
@@ -68,7 +68,6 @@ if((["/", "/index.html"]).indexOf(window.location.pathname) != -1){
 					let e = document.createElement("button");
 					e.classList.add("mdui-btn", "mdui-ripple");
 					e.appendChild(document.createTextNode("确认"));
-					e.setAttribute("mdui-dialog-close", "");
 					e.addEventListener("click", event => {
 						let us = accountSettingsDialog.$element[0].children[1].children[1].value;
 						if (us != ""){
@@ -76,6 +75,7 @@ if((["/", "/index.html"]).indexOf(window.location.pathname) != -1){
 						}else{
 							localStorage.removeItem("authInfo");
 						}
+						accountSettingsDialog.close();
 					});
 					return e;
 				})());
