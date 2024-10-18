71d8fb5313c9f7cbaf6d8479376101968fb01dde
农药君 <99505726+cnbilinyj@users.noreply.github.com>
2024-10-18 18:15:39 +0800

    udtms

---
+++ tms-get.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         WebCat共享空间篡改猴脚本工具
+// @namespace    cnbilinyj-webcat
+// @version      2024-10-10
+// @description  WebCat的外接脚本工具
+// @author       cnbilinyj
+// @match        *://space.webcat.top/*
+// @icon         http://x.webcat.top/img/icon.png
+// @grant        none
+// @run-at       document-end
+// @downloadURL  https://cnbilinyj.github.io/WebCat-Tamper-monkey-script-Tools/tms-get.js
+// @updateURL    https://cnbilinyj.github.io/WebCat-Tamper-monkey-script-Tools/tms-get.js
+// ==/UserScript==
+(function() {
+    'use strict';
+    let script = document.createElement("script");
+    script.setAttribute("src", `https://cnbilinyj.github.io/WebCat-Tamper-monkey-script-Tools/tms-main.js?timestamp=${new Date().valueOf()}`);
+    document.documentElement.appendChild(script);
+    // document.write(document.documentElement.outerHTML);
+    // Your code here...
+})();
