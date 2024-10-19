07df65e6a9cf6156bee6161255bbb25878de45b5
农药君 <99505726+cnbilinyj@users.noreply.github.com>
2024-10-19 21:14:09 +0800

    udtms

--- WCSSMAMaS/tms-main-pigeonhole.js
+++ WCSSMAMaS/tms-main-pigeonhole.js
@@ -133,8 +133,8 @@ if((["/", "/index.html"]).indexOf(window.location.pathname) != -1){
 						if (us != ""){
 							Reflect.deleteProperty(authInfos, us);
 							Array.from(select_element.children).forEach((i) => {
-								console.log(i, select_element.children);
-								// i.remove();
+								// console.log(i, select_element.children);
+								i.remove();
 							});
 							Object.keys(authInfos).forEach(item => {
 								let option = document.createElement("option");
